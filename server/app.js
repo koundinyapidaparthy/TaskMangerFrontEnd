af0fbf004177fc7ddf991d9325d6df0b9586c83d
Koundinya Pidaparthy <koundinya.pidaparthy@cityexperiences.com>
2023-06-18 21:05:21 +0530

    updated EnV File Path

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const secure = require("dotenv");
 const bodyParser = require("body-parser");
-secure.config({ path: "./config.env" });
+secure.config({ path: "./.env" });
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
