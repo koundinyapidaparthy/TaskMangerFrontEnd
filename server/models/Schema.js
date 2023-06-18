2f3d550f47e573c8ddb057141b11ab03a9ea4881
Koundinya Pidaparthy <koundinya.pidaparthy@cityexperiences.com>
2023-06-18 20:08:28 +0530

    Adding All Backend Files here

--- server/models/Schema.js
+++ server/models/Schema.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
-const newSchema= new mongoose.Schema({
-    name:{
-        type:String,
-    },
-    email:{
-        type:String
-    },
-    photo:{
-        type:String
-    }
-})
-const MF= new mongoose.model("MF",newSchema);
-module.exports=MF;
+const mongoose = require("mongoose");
+const createUserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  tasks: {
+    type: [
+      {
+        completed: Boolean,
+        task: String,
+      },
+    ],
+  },
+});
+
+const MF = new mongoose.model("MF", createUserSchema);
+
+module.exports = MF;
